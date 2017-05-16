4fffb12ec443b82d07183d28159ebf76d1f276c0
connie-reinholdsson <connie.reinholdsson@gmail.com>
2017-05-16 15:56:59 +0100

    Structured basic functions with branches

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -1,5 +1,9 @@
 class GildedRose
 
+  MINIMUM_QUALITY = 0
+  MAXIMUM_QUALITY = 50
+  SELL_DATE_PASSED = 0
+
   def initialize(items)
     @items = items
   end
@@ -21,53 +25,87 @@ class GildedRose
     end
   end
 
+  def conjured?(item)
+    item.name.include? "conjure"
+  end
+
+  def conjured(item)
+  end
+
+  def minimum_quality?(item)
+    true if item.quality > MINIMUM_QUALITY
+  end
+
+  def sell_date_passed?(item)
+    true if item.sell_in <= SELL_DATE_PASSED
+  end
+
+  def normal_item(item)
+    if minimum_quality?(item) && !sell_date_passed?(item)
+        item.quality = item.quality - 1
+    elsif minimum_quality?(item) && sell_date_passed?(item)
+        item.quality = item.quality - 2
+    end
+  end
+
+
   def update_quality()
     @items.each do |item|
+      item.sell_in = item.sell_in - 1
       if exception?(item)
-          exception
-        if item.quality > 0
-            item.quality = item.quality - 1
-        end
+          exception(item)
+      elsif conjured?(item)
+          conjured(item)
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
+          normal_item(item)
       end
     end
   end
+
 end
+
+
+
+#
+#         if item.quality < 50
+#           item.quality = item.quality + 1
+#           if item.name == "Backstage passes to a TAFKAL80ETC concert"
+#             if item.sell_in < 11
+#               if item.quality < 50
+#                 item.quality = item.quality + 1
+#               end
+#             end
+#             if item.sell_in < 6
+#               if item.quality < 50
+#                 item.quality = item.quality + 1
+#               end
+#             end
+#           end
+#         end
+#       end
+#       if item.name != "Sulfuras, Hand of Ragnaros"
+#         item.sell_in = item.sell_in - 1
+#       end
+#       if item.sell_in < 0
+#         if item.name != "Aged Brie"
+#           if item.name != "Backstage passes to a TAFKAL80ETC concert"
+#             if item.quality > 0
+#               if item.name != "Sulfuras, Hand of Ragnaros"
+#                 item.quality = item.quality - 1
+#               end
+#             end
+#           else
+#             item.quality = item.quality - item.quality
+#           end
+#         else
+#           if item.quality < 50
+#             item.quality = item.quality + 1
+#           end
+#         end
+#       end
+#     end
+#   end
+# end
 
 
 
--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -5,25 +5,34 @@ describe GildedRose do
 subject(:gilded_rose) { described_class.new()}
 let (:aged_brie) { double :aged_brie}
 
-  describe "#update_quality"
-    context 'normal items' do
-      it "quality should decrease in sell_in by 1 unit each day" do
-        apple = Item.new("apple", 10, 15)
-        GildedRose.new([apple]).update_quality
-        expect(apple.sell_in).to eq 9
-    end
-
-      it "sell_in should decrease in quality by 1 unit each day " do
+  describe "#normal_item" do
+      it "quality should decrease by 1 unit before sell date has passed" do
         apple = Item.new("apple", 10, 15)
         GildedRose.new([apple]).update_quality
         expect(apple.quality).to eq 14
       end
+
+      it "quality should decrease by 2 units after sell date has passed" do
+        apple = Item.new("apple", 0, 15)
+        GildedRose.new([apple]).update_quality
+        expect(apple.quality).to eq 13
+      end
     end
 
-    describe "#exception?"
+  describe "#update_quality" do
+      it "sell_in should decrease by 1 unit each time update_quality method is run" do
+        apple = Item.new("apple", 10, 15)
+        GildedRose.new([apple]).update_quality
+        expect(apple.sell_in).to eq 9
+      end
+    end
+
+    describe "#exception?" do
     it "should return true if the item is an exception" do
       brie = Item.new("Aged Brie", 10, 15)
       example = GildedRose.new([brie])
       expect(example.exception?(brie)).to eq true
   end
 end
+
+end 
